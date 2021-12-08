02ff57d8bcd87300be2879b49412fa05a8d1013f
scarr <scarr@student.chalmers.se>
2021-12-08 11:43:56 +0100

    Filled Repositories

--- src/repositories/EmployeeRepository.java
+++ src/repositories/EmployeeRepository.java
@@ -1,4 +1,73 @@
 package repositories;
 
-public class EmployeeRepository {
+import domain.entities.Employee;
+
+import java.util.ArrayList;
+
+public class EmployeeRepository extends AbstractRepository {
+
+
+    public ArrayList<Employee> getAll() {
+        return EmployeeRepository.persistenceData.getEmployees();
+    }
+
+
+    public Employee getBySSN(long SSN) {
+        ArrayList<Employee> employees = EmployeeRepository.persistenceData.getEmployees();
+        Employee foundEmployee = null;
+        for (Employee employee : employees){
+            if (employee.getSSN() == SSN){
+                foundEmployee = employee;
+            }
+        }
+        return foundEmployee;
+    }
+
+
+    public Employee getByAccessToken(String accessToken) {
+        ArrayList<Employee> employees = EmployeeRepository.persistenceData.getEmployees();
+        Employee foundEmployee = null;
+        for (Employee employee : employees){
+            if (employee.getAccessToken() == accessToken){
+                foundEmployee = employee;
+            }
+        }
+        return foundEmployee;
+    }
+
+
+    public Employee getByEmail(String email) {
+        ArrayList<Employee> employees = EmployeeRepository.persistenceData.getEmployees();
+        Employee foundEmployee = null;
+        for (Employee employee : employees){
+            if (employee.getEmail() == email){
+                foundEmployee = employee;
+            }
+        }
+        return foundEmployee;
+    }
+
+
+    public void createProfile(Employee employee) {
+        ArrayList<Employee> employees = EmployeeRepository.persistenceData.getEmployees();
+        employees.add(employee);
+        EmployeeRepository.persistenceData.setEmployees(employees);
+    }
+
+
+    public void updateProfile(Employee employee) {
+        ArrayList<Employee> employees = EmployeeRepository.persistenceData.getEmployees();
+        for (Employee currentEmployee : employees){
+            if (currentEmployee.equals(employee)){
+                currentEmployee.setFullName(employee.getFullName());
+                currentEmployee.setPassword(employee.getPassword());
+                currentEmployee.setEmail(employee.getEmail());
+                currentEmployee.setPhoneNumber(employee.getPhoneNumber());
+                currentEmployee.setBirthDate(employee.getBirthDate());
+                currentEmployee.setAccessToken(employee.getAccessToken());
+                currentEmployee.setPosition(employee.getPosition());
+            }
+        }
+        EmployeeRepository.persistenceData.setEmployees(employees);
+    }
 }
--- src/repositories/PortfolioRepository.java
+++ src/repositories/PortfolioRepository.java
@@ -1,4 +1,37 @@
 package repositories;
 
-public class PortfolioRepository {
+import domain.entities.Portfolio;
+
+import java.util.ArrayList;
+
+public class PortfolioRepository extends AbstractRepository{
+
+    public Portfolio getPortfolioBySSN(long SSN) {
+        ArrayList<Portfolio> portfolios = PortfolioRepository.persistenceData.getPortfolios();
+        Portfolio portfolio = null;
+        for (Portfolio currentPortfolio : portfolios){
+            if (currentPortfolio.getCustomerSSN() == SSN){
+                portfolio = currentPortfolio;
+            }
+        }
+        return portfolio;
+    }
+
+
+    public void createPortfolio(Portfolio portfolio) {
+        ArrayList<Portfolio> portfolios = PortfolioRepository.persistenceData.getPortfolios();
+        portfolios.add(portfolio);
+        PortfolioRepository.persistenceData.setPortfolios(portfolios);
+    }
+
+
+    public void updatePortfolio(Portfolio portfolio) {
+        ArrayList<Portfolio> portfolios = PortfolioRepository.persistenceData.getPortfolios();
+        for (Portfolio currentPortfolio : portfolios){
+            if (currentPortfolio.equals(portfolio)){
+                currentPortfolio.setStocks(portfolio.getStocks());
+            }
+        }
+        PortfolioRepository.persistenceData.setPortfolios(portfolios);
+    }
 }
