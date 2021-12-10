6e7931c1c8c917100d93258b7ebde3ef81489c44
scarr <scarr@student.chalmers.se>
2021-12-10 11:08:04 +0100

    ApplyForMortgage

--- src/usecases/ValidateMortgage.java
+++ src/usecases/ValidateMortgage.java
@@ -4,7 +4,6 @@ package usecases;
 //This method is created for checking if a user is allowed to get a mortgage or not
 
 import domain.entities.BankAccount;
-import domain.entities.Customer;
 import domain.entities.Mortgage;
 import repositories.BankAccountRepository;
 import repositories.CustomerRepository;
@@ -19,13 +18,17 @@ public class ValidateMortgage {
         this.bankAccountRepository = new BankAccountRepository();
     }
 
-    public boolean execute(long accountNumber, Mortgage mortgage){
+    public boolean execute(long accountNumber,double totalMortgageValue, double time){
         BankAccount bankAccount = bankAccountRepository.getAccountByAccountNumber(accountNumber);
-        if (bankAccount.getBalance() < 0.2*mortgage.getTotalValue() ){
+        //Do we need any other validations for
+        if (bankAccount.getBalance() < 0.2 * totalMortgageValue){
             return false;
         }
-
-
+        //The customer cannot ask for a mortgage to pay in +40 years
+        if (time > 40){
+            return false;
+        }
+        return true;
     }
 
 
