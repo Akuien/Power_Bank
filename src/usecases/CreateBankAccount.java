024279ee5e5165a5c6213a31a17922f4bd5a025e
jonna <jonnajoh@net.chalmers.se>
2021-12-13 12:52:53 +0100

    Started on CreateBankAccount.

--- src/usecases/CreateBankAccount.java
+++ src/usecases/CreateBankAccount.java
@@ -1,4 +1,22 @@
 package usecases;
 
+import domain.constants.BankAccountStatus;
+import domain.entities.BankAccount;
+import domain.exceptions.CustomerDoesNotExistException;
+import domain.exceptions.EmployeeDoesNotExistException;
+import repositories.BankAccountRepository;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class CreateBankAccount {
+
+    private ValidateCustomer validateCustomer;
+    private ValidateEmployee validateEmployee;
+    private BankAccountRepository bankAccountRepository;
+
+    public CreateBankAccount() {
+        this.validateCustomer = new ValidateCustomer();
+        this.validateEmployee = new ValidateEmployee();
+        this.bankAccountRepository = new BankAccountRepository();
+    }
 }
