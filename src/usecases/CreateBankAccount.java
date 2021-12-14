ddb2d77218694b2b4f89382551d88e6b6d876eb8
jonna <jonnajoh@net.chalmers.se>
2021-12-14 14:15:42 +0100

    CreateBankAccount.

--- src/usecases/CreateBankAccount.java
+++ src/usecases/CreateBankAccount.java
@@ -19,4 +19,27 @@ public class CreateBankAccount {
         this.validateEmployee = new ValidateEmployee();
         this.bankAccountRepository = new BankAccountRepository();
     }
+
+    public String execute(long employeeSSN, long customerSSN, String bankAccountName) throws Exception {
+        //Checks if the employee exists
+        boolean employeeExists = validateEmployee.execute(employeeSSN);
+        if (!employeeExists){
+            throw new EmployeeDoesNotExistException(employeeSSN);
+        }
+
+        //We check if the customer who is doing the application exists
+        boolean customerExists = validateCustomer.execute(customerSSN);
+        if (!customerExists){
+            throw new CustomerDoesNotExistException(customerSSN);
+        }
+
+        //With this method we can generate an accountNumber
+        long accountNumber = ThreadLocalRandom.current().nextLong(100000000,999999999);
+
+        BankAccount bankAccount = new BankAccount(0, bankAccountName, accountNumber, customerSSN);
+        bankAccount.setStatus(BankAccountStatus.approved);
+        bankAccountRepository.createBankAccount(bankAccount);
+        return "Bank account with " + accountNumber + " created successfully.";
+    }
+
 }
