a7fc5602677ca43aee4bf0665d905d95c0619ee0
scarr <scarr@student.chalmers.se>
2021-12-16 13:54:49 +0100

    Merge Conflict

--- src/domain/exceptions/IncorrectDateFormatException.java
+++
@@ -1,7 +0,0 @@
-package domain.exceptions;
-
-public class IncorrectDateFormatException extends Exception{
-    public IncorrectDateFormatException(String birthDate){
-        super("The date " + birthDate + " is not formatted correctly, please use the following format: dd/mm/yyyy.");
-    }
-}
