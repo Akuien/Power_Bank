2d726cfc66546e22296385129cc082e51665608f
scarr <scarr@student.chalmers.se>
2021-12-17 17:21:54 +0100

    Customer Menu LogIn

--- src/main/java/controllers/CustomerMenuLogIn.java
+++ src/main/java/controllers/CustomerMenuLogIn.java
@@ -1,49 +1,82 @@
 package controllers;
 
 import usecases.LogInCustomer;
+import usecases.RegisterCustomer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CustomerMenuLogIn {
 
     private static LogInCustomer logInCustomer;
     private MainMenu mainmenu;
+    private RegisterCustomer registerCustomer;
 
     public CustomerMenuLogIn(MainMenu mainmenu) {
+
         this.mainmenu = mainmenu;
+        this.registerCustomer = new RegisterCustomer();
     }
 
 
-    public void MenuLogInCustomer() {
+    public void printMenuLogInCustomer() {
 
-        int option = UserInput.inputInt("Log In Menu for Customer:" + System.lineSeparator() +
+        System.out.println("Log In Menu for Customer:" + System.lineSeparator() +
                 "0. Return to Main Menu" + System.lineSeparator() +
                 "1. Log in " + System.lineSeparator() +
                 "2. Create new account ");
+    }
 
-        while (option < 0 || option > 8) {
+    public void customerMenuLogIn(int option){
 
-            option = UserInput.inputInt("Invalid option");
+        do {
+            switch (option) {
 
-        }
-        switch (option) {
+                case 0: mainmenu.Menu();
+                    break;
 
-            case 0: mainmenu.Menu();
-                break;
+                case 1:
+                    try {
+                        String email =UserInput.inputString(" Enter Email: ");
+                        String password = UserInput.inputString(" Enter Password: ");
+                        logInCustomer.execute(email, password);
+                        printMenuLogInCustomer();
+                        option = UserInput.inputInt("Enter an option: ");
+                    }
 
-            case 1: try {
-                String email =UserInput.inputString(" Enter Email: ");
-                String password = UserInput.inputString(" Enter Password: ");
-                logInCustomer.execute(email, password);
-            } catch (Exception exception){
-                System.out.println(exception.getMessage());
+                    catch (Exception exception){
+                    System.out.println(exception.getMessage());
+                }
+                    break;
+
+                case 2:
+                    try {
+                        String firstName = UserInput.inputString("Enter First Name: ");
+                        String lastName = UserInput.inputString("Enter Last Name: ");
+                        long SSN = UserInput.inputLong("Enter SSN: ");
+                        String password = UserInput.inputString("Enter Password: ");
+                        String email = UserInput.inputString("Enter Email: ");
+                        String phoneNumber = UserInput.inputString("Enter Phone Number");
+                        String birthDateString = UserInput.inputString("Enter Birth Date");
+                        Date birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(birthDateString);
+                        registerCustomer.execute(firstName, lastName, SSN, password, email, phoneNumber, birthDate);
+
+                        printMenuLogInCustomer();
+                        option = UserInput.inputInt("Enter an option: ");
+                    }
+                    catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+
+                default:
+                    System.out.println("Please enter valid option");
+                    option = UserInput.inputInt("Enter an option: ");
+                    break;
             }
 
-                break;
-            case 2:
-                ;
-            default:
-                System.out.println("Please enter valid option");
-                break;
-        }
+        }while (option < 0 || option > 8);
+
     }
 
 }
+
