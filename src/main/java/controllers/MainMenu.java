fa748eed8652230084e872f60040e9dee8e8aac5
Aleksey Zorin <aleksey@student.chalmers.se>
2021-12-19 17:15:05 +0100

    Rewriting MainMenu according to proposed modifications.

--- src/main/java/controllers/MainMenu.java
+++ src/main/java/controllers/MainMenu.java
@@ -1,46 +1,11 @@
-package controllers;
-
-import static controllers.CustomerMenu.EOL;
-
-public class MainMenu {
-    private CustomerLogInMenu customerMenuLogIn;
-    private EmployeeLogInMenu employeeMenuLogIn;
+{
+    private CustomerLogInMenu customerLogInMenu;
+    private EmployeeLogInMenu employeeLogInMenu;
 
     public MainMenu() {
-    }
-
-    public void Menu(){
-
-        int option = UserInput.inputInt("MainMenu options menu:" + System.lineSeparator()+
-                "0. Quit" + EOL +
-                "1. Customer Login Menu" + EOL +
-                "2. Employee Login Menu" + EOL +
-                "3. Manager Login Menu");
-
-        while(option < 0 || option > 8){
-
-            option = UserInput.inputInt("Invalid option");
-
-        }switch(option){
-
-            case 0: // Terminate Program
-                break;
-            case 1 : customerMenuLogIn.printMenuLogInCustomer();
-                break;
-            case 2 : employeeMenuLogIn.MenuLogInEmployee();
-                break;
-            case 3 : managerMenuLogIn.MenuLogInManager();
-
-            default: System.out.println("Please enter valid option");
-                break;
+        this.customerLogInMenu = new CustomerLogInMenu();
+        this.employeeLogInMenu = new EmployeeLogInMenu();
         }
 
-    }
-
-    public static void main(String[] args) {
-
-        MainMenu menuClass = new MainMenu();
-        menuClass.Menu();
-
-    }
-}
+         
+        }
