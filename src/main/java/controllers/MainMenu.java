f76ef46ed933df92f7afa17f005c6d78fdc80ae4
Aleksey Zorin <aleksey@student.chalmers.se>
2021-12-19 17:32:08 +0100

    Rewriting MainMenu according to proposed modifications.

--- src/main/java/controllers/MainMenu.java
+++ src/main/java/controllers/MainMenu.java
@@ -7,5 +7,24 @@
         this.employeeLogInMenu = new EmployeeLogInMenu();
         }
 
-         
+        public void printMenu() {
+
+        System.out.println("Main Menu options menu:" + System.lineSeparator() +
+              "0. Quit" + EOL +
+              "1. Customer Login Menu" + EOL +
+              "2. Employee Login Menu");
+        }
+        public void menu(int option){
+        do {
+            switch (option) {
+
+                case 0:
+        //This line closes the whole system, and we take a 0 as a parameter because it has executed in a successful way
+        //If the parameter had been 1 or -1 it meant it was exited in an unsuccessful way.
+
+        }
+        }
+        }
+
+
         }
