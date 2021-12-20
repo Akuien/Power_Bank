d4239b20bd2bfa6249ac4c68bfd7e00850548e64
Akuien <91293028+Akuien@users.noreply.github.com>
2021-12-20 12:07:34 +0100

    Refactoring the ManagersMenu

--- src/main/java/controllers/ManagerMenu.java
+++ src/main/java/controllers/ManagerMenu.java
@@ -1,111 +1,31 @@
 package controllers;
 
-import repositories.EmployeeRepository;
-import usecases.*;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import static controllers.CustomerMenu.EOL;
 
 public class ManagerMenu {
 
-    private RegisterEmployee registerEmployee;
-    private ObtainEmployees obtainEmployees;
-    private EmployeeMenu employeeMenu;
-    private MainMenu mainMenu;
-    private EmployeeRepository employeeRepository;
-    private PromoteEmployee promoteEmployee;
-    private LogOutEmployee logOutEmployee;
+        public void printMenu() {
+
+            System.out.println("Employee menu:" + System.lineSeparator() +
+                    "0. Log Out." + EOL +
+                    "1. List customers" + EOL +
+                    "2. List customer's bank account(s)" + EOL +
+                    "3. List mortgage applications." + EOL +
+                    "4. List bank account applications." + EOL +
+                    "5. List employees." + EOL +
+                    "6. Change status mortgage." + EOL +
+                    "7. Change status bank account." + EOL +
+                    "8. Promote employee." + EOL +
+                    "9. Create employee account");
+        }
 
 
-    /*private*/
-
-
-    public ManagerMenu() {
-        this.registerEmployee = new RegisterEmployee();
-        this.employeeMenu = new EmployeeMenu();
-        this.mainMenu = new MainMenu();
-        this.obtainEmployees = new ObtainEmployees();
-        this.employeeRepository = new EmployeeRepository();
-    }
-
-    public void printManagerMenu() {
-
-        int option = UserInput.inputInt("Log In Menu for Manager:" + EOL +
-                "0. Return to Main Menu" + EOL +
-                "1. Employee Menu " + EOL +
-                "2. Register Employee." + EOL +
-                "3. Obtain Employee by SSN. " + EOL +
-                "4. Promote Employee." + EOL +
-                "5. Show all employees. " + EOL +
-                "6. Log Out. ");
-        //int input = UserInput.inputInt("Choose an option: ");
-    }
-
-    public void managerMainMenu(int option){
-            do {
-                switch (option) {
-                    case 0:
-                        mainMenu.Menu();
-                        break;
-                    case 1:
-                        employeeMenu.PrintEmployeeMenu();
-                        break;
-                    case 2:
-                        try {
-                            String firstName = UserInput.inputString("Enter the first name: ");
-                            String lastName = UserInput.inputString("Enter the last name: ");
-                            long SSN = UserInput.inputLong("Enter the ssn: ");
-                            String password = UserInput.inputString("Enter the password: ");
-                            String email = UserInput.inputString("Enter the email: ");
-                            String phoneNumber = UserInput.inputString("Enter the phone number: ");
-                            String inputBirthDate = UserInput.inputString("Enter the birthdate: ");
-                            Date birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(inputBirthDate);
-                            registerEmployee.execute(firstName, lastName, SSN, password, email, phoneNumber, birthDate);
-                            option = UserInput.inputInt("Enter the desired option: ");
-                        }
-                        catch (Exception exception){
-                            System.out.println(exception.getMessage());
-                        }
-                        break;
-
-                    case 3:
-                        try {
-                            long ssn = UserInput.inputLong("Enter ssn of the employee: ");
-                            employeeRepository.getBySSN(ssn);
-                        }
-                        catch(Exception exception){
-                            System.out.println(exception.getMessage());
-                        }
-
-                        break;
-                    case 4:
-                        try{
-                        long employeeSSN = UserInput.inputLong("Enter ssn of the employee: ");
-                        long managerSSN = UserInput.inputLong("Enter ssn of the manager: ");
-                        promoteEmployee.execute(managerSSN, employeeSSN);
-                        }
-                        catch(Exception exception){
-                            System.out.println(exception.getMessage());
-                        }
-                        break;
-                    case 5:
-                        employeeRepository.getAll();
-                        break;
-                    case 6:
-                        try{
-                        String accessToken = UserInput.inputString(" ");
-                        logOutEmployee.execute(accessToken);}
-                        catch(Exception exception){
-                            System.out.println(exception.getMessage());
-                        }
-                        break;
-
-                    default:
-                        System.out.println("Please enter valid option");
-                        break;
-                }
-            } while (option < 0 || option > 6);
-    }
 }
+
+
+
+
+
+
+
+
