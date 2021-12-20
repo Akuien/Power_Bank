e0efce80ab6fa5cdeed19cfda1e46c57b249c745
scarr <scarr@student.chalmers.se>
2021-12-20 21:51:46 +0100

    Fixed mistake

--- src/main/java/usecases/ObtainOwnedStocks.java
+++ src/main/java/usecases/ObtainOwnedStocks.java
@@ -1,2 +1,20 @@
-package usecases;public class ObtainOwnedStocks {
+package usecases;
+
+import domain.entities.Portfolio;
+import domain.entities.Stock;
+import repositories.PortfolioRepository;
+
+import java.util.ArrayList;
+
+public class ObtainOwnedStocks {
+    private PortfolioRepository portfolioRepository;
+
+    public ObtainOwnedStocks(){
+        this.portfolioRepository = new PortfolioRepository();
+    }
+
+    public ArrayList<Stock> execute(long customerSSN){
+        Portfolio ownedPortfolio = portfolioRepository.getPortfolioBySSN(customerSSN);
+        return ownedPortfolio.getStocks();
+    }
 }
