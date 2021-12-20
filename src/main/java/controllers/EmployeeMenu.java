78d9f608abc65edad14d0f76bdb3538402fec327
d0nate110 <anton.golubenko@gmail.com>
2021-12-20 11:40:51 +0100

    Changed and finished with EmployeeMenu class, and added ObtainPendingMortgages and ObtainPendingBankAccounts classes.

--- src/main/java/controllers/EmployeeMenu.java
+++ src/main/java/controllers/EmployeeMenu.java
@@ -2,140 +2,155 @@ package controllers;
 
 import data.PersistenceData;
 import domain.constants.MortgageStatus;
+import domain.entities.BankAccount;
 import domain.entities.Customer;
+import domain.entities.Employee;
 import domain.entities.Mortgage;
 import usecases.*;
 
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import static controllers.CustomerMenu.EOL;
 
 public class EmployeeMenu {
-    private LogInEmployee logInEmployee;
+
     private LogOutEmployee logOutEmployee;
-    private RegisterCustomer registerCustomer;
-    private ValidateCustomer validateCustomer;
-    private ValidateCustomerBankAccount validateCustomerBankAccount;
-    private CustomerMenu customerMenu;
-    private ValidateEmail validateEmail;
-    private CheckTransactionHistory checkTransactionHistory;
-    private PersistenceData listOfCustomers;
+    private ObtainCustomers obtainCustomers;
+    private ObtainCustomerBankAccounts obtainCustomerBankAccounts;
+    private ObtainPendingMortgages obtainPendingMortgages;
+    private ObtainPendingBankAccounts obtainPendingBankAccounts;
+    private ChangeStatusMortgage changeStatusMortgage;
+    private ChangeStatusBankAccount changeStatusBankAccount;
+
+    public EmployeeMenu(){
+        this.logOutEmployee = new LogOutEmployee();
+        this.obtainCustomers = new ObtainCustomers();
+        this.obtainCustomerBankAccounts = new ObtainCustomerBankAccounts();
+        this.obtainPendingMortgages = new ObtainPendingMortgages();
+        this.obtainPendingBankAccounts = new ObtainPendingBankAccounts();
+        this.changeStatusMortgage = new ChangeStatusMortgage();
+        this.changeStatusBankAccount = new ChangeStatusBankAccount();
+    }
+
+    public void printMenu() {
 
 
-
-    public void PrintEmployeeMenu() {
-
-
-        int option = UserInput.inputInt("Employee menu:" + System.lineSeparator() +
+        System.out.println("Employee menu:" + System.lineSeparator() +
                 "0. Log Out." + EOL +
-                "1. Register Customer." + EOL +
-                "2. Validate customer." + EOL +
-                "3. Validate Customer bank account." + EOL +
-                "4. Approve mortgage." + EOL +
-                "5. Customer Menu." + EOL +
-                "6. Validate Email.  " + EOL +
-                "7. Look at transaction history." + EOL +
-                "8. " + EOL +
+                "1. List customers" + EOL +
+                "2. List customer's bank account(s)" + EOL +
+                "3. List mortgage applications." + EOL +
+                "4. List bank account applications." + EOL +
+                "5. Change status mortgage." + EOL +
+                "6. Change status bank account." + EOL +
                 "Type an option number: ");
     }
 
-    public void employeeMainMenu(int option) {
+    public void menu(int option, Employee employee) {
 
         do {
             switch (option) {
-
+                //In order for the user to log out, first we call the access token from employee class, and then logOutEmployee class.
                 case 0:
                     try {
-                        String accessToken = UserInput.inputString(" ");
+                        String accessToken = employee.getAccessToken();
                         logOutEmployee.execute(accessToken);
                     } catch (Exception exception) {
                         System.out.println(exception.getMessage());
-
                     }
                     break;
+                // Customer list address is now pointing to obtainCustomers execute method,
+                // and then the for loop loops through the list and print out every customer.
                 case 1:
                     try {
-                        String firstName = UserInput.inputString("Enter the first name of the customer: ");
-                        String lastName = UserInput.inputString("Enter the last name of the customer: ");
-                        long ssn = UserInput.inputLong("Enter ssn of the customer: ");
-                        String password = UserInput.inputString("Enter the password of the customer: ");
-                        String email = UserInput.inputString("Enter the email of the customer: ");
-                        String phoneNumber = UserInput.inputString("Enter the password of the customer: ");
-                        String inputBirthDate = UserInput.inputString("Enter the birthdate: ");
-                        Date birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(inputBirthDate);
-                        registerCustomer.execute(firstName, lastName, ssn, password, email, phoneNumber, birthDate);
-
+                        ArrayList<Customer> customersList = obtainCustomers.execute();
+                        for (Customer currentCustomer : customersList){
+                            System.out.println(currentCustomer.toString());
+                        }
 
                     } catch (Exception exception) {
                         System.out.println(exception.getMessage());
                     }
                     break;
+                // take input from the user, and loop through all the bank accounts in the list and print them out.
                 case 2:
                     try {
-                        long ssn = UserInput.inputLong("Enter ssn of the customer: ");
-
-                        validateCustomer.execute(ssn);
+                        long customerSSN = UserInput.inputLong("Enter the customer's SSN: ");
+                        ArrayList<BankAccount> customersBankAccounts = obtainCustomerBankAccounts.execute(customerSSN);
+                        for (BankAccount currentBankAccount : customersBankAccounts){
+                            System.out.println(currentBankAccount.toString());
+                        }
                     } catch (Exception exception) {
                         System.out.println(exception.getMessage());
                     }
                     break;
+                // loop through the list of pending mortgages and print each one of them.
                 case 3:
-                    try {
-                        long ssn = UserInput.inputLong("Enter ssn of the customer: ");
-                        long bankAcc = UserInput.inputLong("Enter bank account of the customer: ");
-                        validateCustomerBankAccount.execute(ssn, bankAcc);
-                    } catch (Exception exception) {
+                    try{
+                        ArrayList<Mortgage> pendingMortgages = obtainPendingMortgages.execute();
+                        for (Mortgage currentMortgage : pendingMortgages){
+                            System.out.println(currentMortgage.toString());
+                        }
+                    }
+                    catch (Exception exception){
                         System.out.println(exception.getMessage());
                     }
                     break;
+                // loop through the list of pending bank accounts and print them out.
                 case 4:
-                    try {
-                    /*Code semantics: If there exists a customer with the entered id, and if his mortgage status is pending,
-                    change the status to approved.*/
-                        //the customer should have a mortgage as a component inside the customer, in order for this to work.
-                   /* long ssn = UserInput.inputLong("Enter ssn of the customer: ");
-                    if(listOfCustomers.getCustomers().contains(ssn)) {
-                        if()
-                    }*/
-                    } catch (Exception exception) {
+                    try{
+                        ArrayList<BankAccount>pendingBankAccounts = obtainPendingBankAccounts.execute();
+                        for (BankAccount currentBankAccount : pendingBankAccounts){
+                            System.out.println(currentBankAccount.toString());
+                        }
+                    }
+                    catch (Exception exception){
                         System.out.println(exception.getMessage());
                     }
                     break;
+                // entering all the values for changing the mortgage status, and then calling the change mortgage status class.
                 case 5:
                     try {
-                        customerMenu.CustomerMenu();
+                        long customerSSN = UserInput.inputLong("Enter the customer's SSN: ");
+                        long accountNumber = UserInput.inputLong("Enter the customer's bank account number: ");
+                        long mortgageLoanID = UserInput.inputInt("Enter the mortgage ID: ");
+                        long employeeSSN = employee.getSSN();
+                        String choice = UserInput.inputString("Enter the next status of the mortgage (Accepted/Rejected)");
+
+                        String message = changeStatusMortgage.execute(customerSSN, accountNumber, mortgageLoanID, employeeSSN, choice);
+                        System.out.println(message);
+
                     } catch (Exception exception) {
                         System.out.println(exception.getMessage());
                     }
                     break;
+                // entering all the values for changing the bank account status, and the calling change bank account status class.
                 case 6:
                     try {
-                        String email = UserInput.inputString("Enter the email of the customer: ");
-                        validateEmail.execute(email);
+                        long customerSSN = UserInput.inputLong("Enter the customer's SSN: ");
+                        long accountNumber = UserInput.inputLong("Enter the account number of the desired bank account: ");
+                        long employeeSSN = employee.getSSN();
+                        String choice = UserInput.inputString("Enter the next status of the bank account (Accepted/Rejected)");
+
+                        String message = changeStatusBankAccount.execute(accountNumber, customerSSN, employeeSSN, choice);
+                        System.out.println(message);
+
                     } catch (Exception exception) {
                         System.out.println(exception.getMessage());
                     }
                     break;
-                case 7:
-                    try {
-                        long ssn = UserInput.inputLong("Enter ssn of the customer: ");
-                        long accNum = UserInput.inputLong("Enter account number of the customer: ");
-                        checkTransactionHistory.execute(ssn, accNum);
-                    } catch (Exception exception) {
-                        System.out.println(exception.getMessage());
-                    }
-                    break;
+                // if user enters anything above 6, below 0 or not a digit, then the system will ask the user to print out a valid option.
                 default:
                     System.out.println("Please enter valid option");
+                    option = UserInput.inputInt("Enter an option: ");
                     break;
             }
-        } while (option < 0 || option > 6);
-
+        } while (option != 0);
     }
 }
-
 
 
 
--- src/main/java/repositories/AbstractRepository.java
+++ src/main/java/repositories/AbstractRepository.java
@@ -2,7 +2,7 @@ package repositories;
 
 import data.PersistenceData;
 
-public abstract class AbstractRepository {
+public abstract class bstractRepository {
     //We created this abstract class so that the data does not get
     //rebooted every time we get to use each repository
     //Conclusion: we can share data between repositories
