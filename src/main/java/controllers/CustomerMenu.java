3e8a91dab7b6e0a5f260d189315c977bb6258694
Aleksey Zorin <aleksey@student.chalmers.se>
2021-12-20 20:29:39 +0100

    CustomerMenu finished and updated.

--- src/main/java/controllers/CustomerMenu.java
+++ src/main/java/controllers/CustomerMenu.java
@@ -1,6 +1,6 @@
 package controllers;
 
-import.domain.entities.BankAccount;
+import domain.entities.BankAccount;
 import domain.entities.Customer;
 import domain.entities.Transaction;
 import repositories.BankAccountRepository;
@@ -12,7 +12,7 @@ public class CustomerMenu {
     private LogOutCustomer logOutCustomer;
     private CheckBalance checkBalance;
     private ApplyForBankAccount applyForBankAccount;
-    private AppleForMortgage appleForMortgage;
+    private ApplyForMortgage applyForMortgage;
     private TransferMoneyToAnotherAccount transferMoneyToAnotherAccount;
     private DepositMoney depositMoney;
     private WithdrawMoney withdrawMoney;
@@ -27,7 +27,7 @@ public class CustomerMenu {
         this.depositMoney = new DepositMoney();
         this.withdrawMoney = new WithdrawMoney();
         this.checkBalance = new CheckBalance();
-        this.appleForMortgage = new ApplyForMortgage();
+        this.applyForMortgage = new ApplyForMortgage();
         this.applyForBankAccount = new ApplyForBankAccount();
         this.obtainCustomerBankAccounts = new ObtainCustomerBankAccounts();
         this.checkTransactionHistory = new CheckTransactionHistory();
@@ -46,7 +46,6 @@ public class CustomerMenu {
                 "7. List of Bank Accounts" + EOL +
                 "8. Check Transaction History" + EOL +
                 "Type an option number: ");
-        // Dot or no dot? End of each option text.
 
     }
 
@@ -74,113 +73,112 @@ public class CustomerMenu {
                         double balance = transferMoneyToAnotherAccount.execute(originSSN, originAccountNumber, finalSSN, finalAccountNumber, amount);
                         System.out.println("Your balance after the operation is: " + balance);
 
-                        { catch(Exception exception){
-                            System.out.println(exception.getMessage());
-                        }
-                            break;
-
-                            case 2:
-                                try {
-                                    long SSN = customer.getSSN();
-                                    long accountNumber = UserInput.inputInt(" Enter Account Number: ");
-                                    double amount = UserInput.inputDouble(" Enter Amount: ");
-
-                                    double balance = depositMoney.execute(SSN, accountNumber, amount);
-                                    System.out.println(exception.getMessage());
-                                }
-                                break;
-
-                            case 3:
-                                try {
-                                    long SSN = customer.getSSN();
-                                    long accountNumber = UserInput.inputLong(" Enter Account Number: ");
-                                    double amount = UserInput.inputDouble(" Enter Amount: ");
-
-                                    double balance = withdrawMoney.execute(SSN, accountNumber, amount);
-                                    System.out.println("Your balance after the operation is: " + balance);
-
-                                } catch (Exception exception) {
-                                    System.out.println(exception.getMessage());
-                                }
-                                break;
-
-                            case 4:
-                                try {
-                                    long SSN = customer.getSSN();
-                                    long accountNumber = UserInput.inputLong(" Enter Account Number: ");
-
-                                    double balance = checkBalance.execute(SSN, accountNumber);
-                                    System.out.println("Your balance is: " + balance);
-
-                                } catch (Exception exception) {
-                                    System.out.println(exception.getMessage());
-                                }
-                                break;
-
-                            case 5:
-                                try {
-                                    long SSN = customer.getSSN();
-                                    long accountNumber = UserInput.inputLong(" Enter account number: ");
-                                    double totalMortgageValue = UserInput.inputDouble("Enter the total amount of the mortgage: ");
-                                    double years = UserInput.inputDouble("Enter amount of years to pay off mortgage: ");
-                                    double initialDeposit = UserInput.inputDouble(" Enter the initial deposit: ");
-
-                                    String message = applyForMortgage.execute(SSN, accountNumber, totalMortgageValue, years, initialDeposit);
-                                    System.out.println(message);
-
-                                    { catch(Exception exception){
-                                        System.out.println(exception.getMessage());
-                                    }
-                                        break;
-
-                                        case 6:
-                                            try {
-                                                long SSN = customer.getSSN();
-                                                String accountName = UserInput.inputString("Enter the bank account name: ");
-
-                                                String message = applyForBankAccount.execute(SSN, accountName);
-                                                System.out.println(message);
-
-                                            } catch (Exception exception) {
-                                                System.out.println(exception.getMessage());
-                                            }
-                                            break;
-
-                                        case 7:
-                                            try {
-                                                long SSN = customer.getSSN();
-                                                ArrayList<BankAccount> customerBankAccounts = obtainCustomerBankAccounts.execute(SSN);
-                                                for (BankAccount currentBankAccount : customerBankAccounts) {
-                                                    System.out.println(currentBankAccount.toString());
-                                                }
-                                            } catch (Exception exception) {
-                                                System.out.println(exception.getMessage());
-                                            }
-                                            break;
-
-                                        case 8:
-                                            try {
-                                                long SSN = customer.getSSN();
-                                                long accountNumber = UserInput.inputLong("Enter the number of bank account: ");
-                                                ArrayList<Transaction> transactionsList = checkTransactionHistory.execute(SSN, accountNumber);
-                                                for (Transaction currentTransaction : transactionsList) {
-                                                    System.out.println(currentTransaction.toString());
-                                                }
-                                            } catch (Exception exception) {
-                                                System.out.println(exception.getMessage());
-                                            }
-                                            break;
-
-                                        default:
-                                            System.out.println("Please enter valid option!");
-                                            option = UserInput.inputInt("Enter an option: ");
-                                            break;
-                                    }
-
-                                } while (option != 0) ;
-                        }
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
                     }
+                    break;
+
+                case 2:
+                    try {
+                        long SSN = customer.getSSN();
+                        long accountNumber = UserInput.inputInt(" Enter Account Number: ");
+                        double amount = UserInput.inputDouble(" Enter Amount: ");
+
+                        double balance = depositMoney.execute(SSN, accountNumber, amount);
+                        System.out.println("Your balance after the operation is: " + balance);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                case 3:
+                    try {
+                        long SSN = customer.getSSN();
+                        long accountNumber = UserInput.inputLong(" Enter Account Number: ");
+                        double amount = UserInput.inputDouble(" Enter Amount: ");
+
+                        double balance = withdrawMoney.execute(SSN, accountNumber, amount);
+                        System.out.println("Your balance after the operation is: " + balance);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                case 4:
+                    try {
+                        long SSN = customer.getSSN();
+                        long accountNumber = UserInput.inputLong(" Enter Account Number: ");
+
+                        double balance = checkBalance.execute(SSN, accountNumber);
+                        System.out.println("Your balance is: " + balance);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                case 5:
+                    try {
+                        long SSN = customer.getSSN();
+                        long accountNumber = UserInput.inputLong(" Enter account number: ");
+                        double totalMortgageValue = UserInput.inputDouble("Enter the total amount of the mortgage: ");
+                        double years = UserInput.inputDouble("Enter amount of years to pay off mortgage: ");
+                        double initialDeposit = UserInput.inputDouble(" Enter the initial deposit: ");
+
+                        String message = applyForMortgage.execute(SSN, accountNumber, totalMortgageValue, years, initialDeposit);
+                        System.out.println(message);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                case 6:
+                    try {
+                        long SSN = customer.getSSN();
+                        String accountName = UserInput.inputString("Enter the bank account name: ");
+
+                        String message = applyForBankAccount.execute(SSN, accountName);
+                        System.out.println(message);
+
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                case 7:
+                    try {
+                        long SSN = customer.getSSN();
+                        ArrayList<BankAccount> customerBankAccounts = obtainCustomerBankAccounts.execute(SSN);
+                        for (BankAccount currentBankAccount : customerBankAccounts) {
+                            System.out.println(currentBankAccount.toString());
+                        }
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                case 8:
+                    try {
+                        long SSN = customer.getSSN();
+                        long accountNumber = UserInput.inputLong("Enter the number of bank account: ");
+                        ArrayList<Transaction> transactionsList = checkTransactionHistory.execute(SSN, accountNumber);
+                        for (Transaction currentTransaction : transactionsList) {
+                            System.out.println(currentTransaction.toString());
+                        }
+                    } catch (Exception exception) {
+                        System.out.println(exception.getMessage());
+                    }
+                    break;
+
+                default:
+                    System.out.println("Please enter valid option!");
+                    option = UserInput.inputInt("Enter an option: ");
+                    break;
             }
-        }
+
+        } while (option != 0);
     }
 }
