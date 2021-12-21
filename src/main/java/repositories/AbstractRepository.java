1219a75ca17c03b6b59b52b09ebffe06401ed47b
Aleksey Zorin <aleksey@student.chalmers.se>
2021-12-21 16:30:39 +0100

    Added explanation inside "explanation". Halfway though with writing comments about our Repositories, the logic behind them and how they function.

--- src/main/java/repositories/AbstractRepository.java
+++ src/main/java/repositories/AbstractRepository.java
@@ -1,12 +1,13 @@
 package repositories;
 
-import data.PersistenceData; // What is this? (Aleksey)
+import data.PersistenceData; // PersistenceData is our database where all the data is stored.
 
 public abstract class AbstractRepository {
 
     // We created this abstract class so that the data does not get
-    // rebooted every time we get to use each repository
-    // Conclusion: we can share data between repositories
+    // rebooted every time we get to use each repository.
+
+    // Conclusion: we can share data between repositories.
     // This is the "main" Repository, as the other repositories inherit from it.
 
     protected static PersistenceData persistenceData = new PersistenceData();
