b10125a951b4303fd828fc3c9f9f59cca37b55a0
Aleksey Zorin <aleksey@student.chalmers.se>
2021-12-21 15:42:10 +0100

    Created new branch for adding comments to our Repositories. Currently doing so.

--- src/main/java/repositories/AbstractRepository.java
+++ src/main/java/repositories/AbstractRepository.java
@@ -1,10 +1,13 @@
 package repositories;
 
-import data.PersistenceData;
+import data.PersistenceData; // What is this? (Aleksey)
 
 public abstract class AbstractRepository {
-    //We created this abstract class so that the data does not get
-    //rebooted every time we get to use each repository
-    //Conclusion: we can share data between repositories
+
+    // We created this abstract class so that the data does not get
+    // rebooted every time we get to use each repository
+    // Conclusion: we can share data between repositories
+    // This is the "main" Repository, as the other repositories inherit from it.
+
     protected static PersistenceData persistenceData = new PersistenceData();
 }
--- src/main/java/repositories/BankAccountRepository.java
+++ src/main/java/repositories/BankAccountRepository.java
@@ -4,10 +4,10 @@ import domain.entities.BankAccount;
 
 import java.util.ArrayList;
 
-public class BankAccountRepository extends AbstractRepository {
+public class BankAccountRepository extends AbstractRepository { // Each repository outside AbstractRepository is a child class of it.
 
 
-    public BankAccount getAccountByName(String accountName) {
+    public BankAccount getAccountByName(String accountName) { // Method that checks all bank accounts to find an account with corresponding name.
         ArrayList<BankAccount> bankAccounts = BankAccountRepository.persistenceData.getBankAccounts();
 
         for (BankAccount currentBankAccount : bankAccounts){
@@ -15,11 +15,11 @@ public class BankAccountRepository extends AbstractRepository {
                 return currentBankAccount;
             }
         }
-        return null;
+        return null; // Returns null when bank accounts of that name doesn't exist.
     }
 
 
-    public BankAccount getAccountByAccountNumber(long accountNumber) {
+    public BankAccount getAccountByAccountNumber(long accountNumber) { // Same as above, but searches by account number.
         ArrayList<BankAccount> bankAccounts = BankAccountRepository.persistenceData.getBankAccounts();
 
         for (BankAccount currentBankAccount : bankAccounts){
@@ -31,9 +31,9 @@ public class BankAccountRepository extends AbstractRepository {
     }
 
 
-    public ArrayList<BankAccount> getAccountsBySSN(long SSN) {
+    public ArrayList<BankAccount> getAccountsBySSN(long SSN) { // Method to search account by SSN number.
         ArrayList<BankAccount> bankAccounts = BankAccountRepository.persistenceData.getBankAccounts();
-        ArrayList<BankAccount> customerBankAccounts = new ArrayList<BankAccount>();
+        ArrayList<BankAccount> customerBankAccounts = new ArrayList<>();
         for (BankAccount currentBankAccount : bankAccounts){
             if (currentBankAccount.getCustomerSSN() == SSN){
                 customerBankAccounts.add(currentBankAccount);
@@ -43,7 +43,7 @@ public class BankAccountRepository extends AbstractRepository {
     }
 
 
-    public ArrayList<BankAccount> getAccountsByStatus(String status) {
+    public ArrayList<BankAccount> getAccountsByStatus(String status) { // Method to search bank accounts by status.
         ArrayList<BankAccount> bankAccounts = BankAccountRepository.persistenceData.getBankAccounts();
         ArrayList<BankAccount> statusBankAccounts = new ArrayList<>();
         for (BankAccount currentBankAccount : bankAccounts){
@@ -57,18 +57,17 @@ public class BankAccountRepository extends AbstractRepository {
 
     public ArrayList<BankAccount> getAll() {
         return BankAccountRepository.persistenceData.getBankAccounts();
-    }
+    } // Method to see all bank accounts.
 
 
-    public void createBankAccount(BankAccount bankAccount) {
+    public void createBankAccount(BankAccount bankAccount) { // Creates new account and adds it to bankAccounts list.
         ArrayList<BankAccount> bankAccounts = BankAccountRepository.persistenceData.getBankAccounts();
         bankAccounts.add(bankAccount);
         BankAccountRepository.persistenceData.setBankAccounts(bankAccounts);
     }
 
 
-    //receives updated Bank Account
-    public void updateBankAccount(BankAccount bankAccount) {
+    public void updateBankAccount(BankAccount bankAccount) { // Receives updated Bank Account.
         ArrayList<BankAccount> bankAccounts = BankAccountRepository.persistenceData.getBankAccounts();
         for (BankAccount currentBankAccount : bankAccounts){
             if (currentBankAccount.equals(bankAccount)){
